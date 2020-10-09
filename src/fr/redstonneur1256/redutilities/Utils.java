1eba185d95da7cb74c59d8846e54fcf1f40a68b1
Redstonneur1256 <silasm2004@outlook.fr>
2020-10-09 22:25:21 +0200

    Hex to bin & Bin to hex

--- src/fr/redstonneur1256/redutilities/Utils.java
+++ src/fr/redstonneur1256/redutilities/Utils.java
@@ -13,14 +13,15 @@ public class Utils {
 
     private static final Gson gson;
     private static final Pattern urlPattern;
-    private static final  String[] units;
+    private static final String[] units;
+    private static final char[] hexCodes;
 
     static {
         gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
         urlPattern = Pattern.compile("^(?i)https?://[a-z0-9]*.[a-z]*.*$");
         units = new String[] {"", "K", "M", "G", "T", "P", "E", "Z, Y"};
+        hexCodes = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
     }
-
 
     public static String toJson(Object object) {
         return gson.toJson(object);
@@ -76,4 +77,24 @@ public class Utils {
         return new String(output.toByteArray());
     }
 
+    public static String binaryToHex(byte[] data) {
+        StringBuilder builder = new StringBuilder(data.length * 2);
+        for (byte b : data) {
+            builder.append(hexCodes[(b >> 4) & 0xF]);
+            builder.append(hexCodes[b & 0xF]);
+        }
+        return builder.toString();
+    }
+    
+    public static byte[] hexToBinary(String hex) {
+        String hexCode = new String(hexCodes);
+        byte[] data = new byte[hex.length() / 2];
+        for(int i = 0; i < hex.length(); i += 2) {
+            int pos1 = hexCode.indexOf(hex.charAt(i));
+            int pos2 = hexCode.indexOf(hex.charAt(i + 1));
+            data[i / 2] = (byte) ((pos1 << 4) | pos2);
+        }
+        return data;
+    }
+    
 }
